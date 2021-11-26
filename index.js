e8316ea09fd8ebc30479474c4c2ee46e59d76b00
Moshior <moshiorrahman279312@gmail.com>
2021-11-26 23:40:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qwz91.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('apointments');
+
+
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            console.log(date);
+            const query = { email: email }
+            console.log(query);
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result)
+        });
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello from Doctors Portal!')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port} `)
+})
+
+
+    // app.get('/users') for get all users
+    // app.get('/users/:id') for get specific id
+    // app.post('/users') for post single user
+    // app.put('/users/:id') for update specific id
+    // app.delete('/users/:id') for delete specific id
